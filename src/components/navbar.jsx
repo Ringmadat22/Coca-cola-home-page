6235d8b9d86dcf0984f3026c98c8036e12d0feb0
Ring Madat <smithemma329@gmail.com>
2024-03-07 20:28:11 +0300

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+
+
+function navbar() {
+    return(
+        <>
+            <nav>
+                <div className="navbar">
+                    
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default navbar

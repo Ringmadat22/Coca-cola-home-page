2066743a191c8896d7f8de7ac6cb7584200233ab
Ring Madat <smithemma329@gmail.com>
2024-03-09 12:57:08 +0300

    main

---
+++ src/components/brands.jsx
@@ -0,0 +1,13 @@
+import '/Users/ringo/Desktop/coca-cola-landing-page/src/App.css';
+
+function Brands() {
+    return(
+        <>
+            <section className='brands-section'>
+                
+            </section>
+        </>
+    )
+}
+
+export default Brands
